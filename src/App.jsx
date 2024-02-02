1bddc1b044315d5fe40e2b09eafa8450745df750
duyuitk16 <nguyenvuanhduy173@gmail.com>
2024-02-03 01:49:11 +0700

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-red-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex incidunt adipisci harum, quia commodi a rem nostrum cum omnis voluptatem eveniet. Molestias temporibus eligendi dolores vel placeat velit odio impedit!</p>
+    </>
+  )
+}
+
+export default App
