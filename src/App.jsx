b446d240d13e8036ebb2cabb361a252c24540538
duyuitk16 <nguyenvuanhduy173@gmail.com>
2024-02-07 03:37:36 +0700

    Code Splitting & Lazy load component

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,35 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './components/layout/Main'
 import "swiper/scss"
 import Banner from './components/banner/Banner'
-import HomePage from './pages/HomePage'
-import MoviePage from './pages/MoviePage'
-import MovieDetail from './pages/MovieDetail'
+// import HomePage from './pages/HomePage'
+// import MoviePage from './pages/MoviePage'
+// import MovieDetail from './pages/MovieDetail'
+import { lazy, Suspense } from 'react'
 
+const HomePage = lazy(() => import('./pages/HomePage'))
+const MoviePage = lazy(() => import('./pages/MoviePage'))
+const MovieDetail = lazy(() => import('./pages/MovieDetail'))
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Main></Main>}>
-          <Route path='/' element={
-            <>
-              <Banner></Banner>
-              <HomePage></HomePage>
-            </>
-          }>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path='/' element={<Main></Main>}>
+            <Route path='/' element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }>
+            </Route>
+            <Route path='/movies' element={<MoviePage></MoviePage>}></Route>
+            <Route path='/movie/:movieId' element={<MovieDetail></MovieDetail>}></Route>
           </Route>
-          <Route path='/movies' element={<MoviePage></MoviePage>}></Route>
-          <Route path='/movie/:movieId' element={<MovieDetail></MovieDetail>}></Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
+
     </>
   )
 }
